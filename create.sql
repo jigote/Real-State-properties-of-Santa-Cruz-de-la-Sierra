d2d77e6a649080f2951de3f9c9efa918bb54b5a0
Renan Añez <renan.anez.merlin@gmail.com>
2020-01-03 14:33:53 -0400

    creé mi primer tabla en la DATABASE de heroku, (nose si hay que hacer commits en estos casos :/

---
+++ create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE departamento (
+    id SERIAL PRIMARY KEY,
+    ambiente VARCHAR NOT NULL,
+    duración INTEGER NOT NULL
+);
---
+++ insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO departamentos (edificio, dirección, duración) VALUES ('Smart Studio', 'calle del Fidalga equipetrol', '480');
+INSERT INTO departamentos (edificio, dirección, duración) VALUES ('Nido', 'Zona norte de Santa Cruz', '352');
+INSERT INTO departamentos (edificio, dirección, duración) VALUES ('Triii', 'zona norte entre 7mo y 8vo anillo, zona el remanso', '721');
+INSERT INTO departamentos (edificio, dirección, duración) VALUES ('Portofino novo', 'Av. La salle, calle claudio peñaranda , zona el remanso', '345');
